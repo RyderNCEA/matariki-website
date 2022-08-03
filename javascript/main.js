3bcb78a1769c02d542c50a238774cb8d5f460cb2
Ryder G <55562141+RyderNCEA@users.noreply.github.com>
2022-08-04 11:38:24 +1200

    Add Comments in main.js
    In this version I added comments into the javascript file to make it easy for future developers and people to read and get an idea of what my code does.

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,8 @@
+// Select Navigation Bar
 var navbar = document.getElementById("nav");
+
+// Make it so when the user scrolls the scrolled class gets added to the navigation class list, 
+// causing it to go from transparent to solid colour.
 window.onscroll = function() {
     "use strict";
     if (document.body.scrollTop >= 65 || document.documentElement.scrollTop >= 65) {
